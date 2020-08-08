dc8e2061f4e1376558c4e1ad310e3e7c0bbfc2b6
Pebanh <peter.banh95@gmail.com>
2020-08-08 14:55:03 -0400

    Edit Header Component to include link back to homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import HeaderStyles from './Header.module.css'
 
 const Header = () => {
   return (
     <div className={HeaderStyles.Header}>
       <div className={HeaderStyles.titleCenter}>
-        <h1 className={HeaderStyles.title}>Minesweeper vs</h1>
+        <Link to="/"><h1 className={HeaderStyles.title}>Minesweeper vs</h1></Link>
       </div>
     </div>
   );
