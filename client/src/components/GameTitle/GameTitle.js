0424b69e8e699181c94e42d31d83011dcab7eab4
Pebanh <peter.banh95@gmail.com>
2020-09-03 11:59:08 -0400

    Refactor GameTitle Component for more reusability

--- client/src/components/GameTitle/GameTitle.js
+++ client/src/components/GameTitle/GameTitle.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import GameTitleStyles from './GameTitle.module.css'
 
-const GameTitle = () => {
+const GameTitle = ({ title, subtitle }) => {
   return (
     <div>
-      <h2 className={GameTitleStyles.title1}>Minesweeper Vs.</h2>
-      <h3 className={GameTitleStyles.title2}>Online Minesweeper Multiplayer</h3>
+      <h2 className={GameTitleStyles.title1}>{title}</h2>
+      {subtitle ? <h3 className={GameTitleStyles.title2}>{subtitle}</h3> : null}
     </div>);
 }
 
