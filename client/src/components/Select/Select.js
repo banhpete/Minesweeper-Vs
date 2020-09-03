0883eed57092864264dc061097b1c12657b5c055
Pebanh <peter.banh95@gmail.com>
2020-09-03 12:08:59 -0400

    Change select component to accept style object

--- client/src/components/Select/Select.js
+++ client/src/components/Select/Select.js
@@ -2,16 +2,23 @@ import React from 'react';
 import SelectStyles from './Select.module.css'
 
 const Select = (props) => {
-  const options = []
+  var options = [];
 
+  /* ---- Deciding Default Value ------------------------------------------------------------------------------------------------ */
   if (props.initial) options.push(<option key="initial" value={props.initial} disabled={true}>{props.initial}</option>)
 
+  /* ---- Adding options ------------------------------------------------------------------------------------------------ */
   props.options.forEach((option) => {
     options.push(<option key={option} value={option}>{option}</option>)
   })
 
   return (
-    <select defaultValue={props.initial ? props.initial : null} onChange={props.handleChange} className={SelectStyles.Select}>
+    <select
+      defaultValue={props.initial ? props.initial : null}
+      onChange={props.handleChange}
+      className={SelectStyles.Select}
+      style={props.style}
+    >
       {options}
     </select>
   );
