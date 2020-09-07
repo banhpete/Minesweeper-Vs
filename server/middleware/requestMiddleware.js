b9b7f9127a4f9cb965fd9d4b40278787cf63010e
Pebanh <peter.banh95@gmail.com>
2020-09-07 16:14:01 -0400

    Add code to troubleshoot Heroku deployment

--- server/middleware/requestMiddleware.js
+++ server/middleware/requestMiddleware.js
@@ -2,6 +2,8 @@ function checkOrigin(req, res, next) {
   if (req.headers.origin === process.env.ORIGIN) {
     next();
   } else {
+    console.log(req.headers.origin);
+    console.log(process.env.ORIGIN)
     let error = new Error("Cannot not process request");
     res.status(400);
     next(error);
