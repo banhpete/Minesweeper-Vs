664b7dd5c1cc3c0bbd03935b71a94c1f6b985988
Pebanh <peter.banh95@gmail.com>
2020-09-07 20:45:07 -0400

    Add disabled status prop in addition to loading prop to disable the button

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ButtonStyle from './Button.module.css'
 
 const Button = (props) => {
+  var disabledStatus = null
+  if (props.loading) {
+    disabledStatus = props.loading
+  } else if (props.disabled) {
+    disabledStatus = props.disabled
+  }
   return (
     <button
       className={props.activated ? ButtonStyle.ButtonActivated : ButtonStyle.Button}
-      style={props.style} disabled={props.loading}
+      style={props.style} disabled={disabledStatus}
       onClick={props.onClick}>
       {props.children}
     </button>
