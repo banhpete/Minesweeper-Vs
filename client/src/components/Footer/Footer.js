b407f4663ebd76bb0cb43fdd212ff7750a794254
Pebanh <peter.banh95@gmail.com>
2020-09-12 19:00:55 -0400

    Add content to footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,7 +3,11 @@ import FooterStyles from './Footer.module.css'
 
 const Footer = () => {
   return (
-    <div className={FooterStyles.Footer}></div>
+    <div className={FooterStyles.Footer}>
+      <p>Made By Peter Banh</p>
+      <a href="https://www.linkedin.com/in/peter-banh/"><i class="fa fa-linkedin-square" style={{ fontSize: 25 }}></i></a>
+      <a href="https://github.com/banhpete/Minesweeper-Vs"><i class="fa fa-github-square" style={{ fontSize: 25 }}></i></a>
+    </div>
   );
 }
 
