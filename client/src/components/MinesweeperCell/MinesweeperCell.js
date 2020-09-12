1c8355991296ade9dbf8704605f8784807d5e372
Pebanh <peter.banh95@gmail.com>
2020-09-12 18:10:53 -0400

    Add styling to minesweeper cells

--- client/src/components/MinesweeperCell/MinesweeperCell.js
+++ client/src/components/MinesweeperCell/MinesweeperCell.js
@@ -3,25 +3,67 @@ import MinesweepercellStyles from './MinesweeperCell.module.css'
 
 const MinesweeperCell = (props) => {
 
+  let numColours = {
+    1: '#0000ff',
+    2: '#008000',
+    3: '#ff0000',
+    4: '#000080',
+    5: '#800000',
+    6: '#008080',
+    7: '#000000',
+    8: '#808080'
+  }
+
+  let color = { color: numColours[props.children] }
+
   let cell = null
   if (props.children === 0) {
     cell =
-      <div index={props.index} style={{ height: props.side, width: props.side }} onClick={props.onClick} onContextMenu={props.onContextMenu} className={MinesweepercellStyles.Cell}></div>
+      <div
+        index={props.index}
+        style={{ height: props.side, width: props.side }}
+        onClick={props.onClick}
+        onContextMenu={props.onContextMenu}
+        className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Opened}`}>
+      </div>
   } else if (props.children === -1) {
     cell =
-      <div index={props.index} style={{ height: props.side, width: props.side }} onClick={props.onClick} onContextMenu={props.onContextMenu} className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Mine}`}>
+      <div index={props.index}
+        style={{ height: props.side, width: props.side }}
+        onClick={props.onClick}
+        onContextMenu={props.onContextMenu}
+        className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Mine}`}>
         {props.children}
       </div>
   } else if (props.children > 0) {
     cell =
-      <div index={props.index} style={{ height: props.side, width: props.side }} onClick={props.onClick} onContextMenu={props.onContextMenu} className={MinesweepercellStyles.Cell}>
-        {props.children}
+      <div
+        index={props.index}
+        style={{ height: props.side, width: props.side }}
+        onClick={props.onClick}
+        onContextMenu={props.onContextMenu}
+        className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Opened}`}>
+        <a style={color}>{props.children}</a>
       </div>
   } else if (props.children === 'f') {
-    cell = <div index={props.index} style={{ background: 'black', color: 'white', height: props.side, width: props.side }} onClick={props.onClick} onContextMenu={props.onContextMenu} className={MinesweepercellStyles.Cell}>f</div>
+    cell = <div
+      index={props.index}
+      className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Flag}`}
+      style={{ height: props.side, width: props.side }}
+      onClick={props.onClick}
+      onContextMenu={props.onContextMenu}
+      f
+    >
+    </div>
   } else {
     cell =
-      <div index={props.index} style={{ height: props.side, width: props.side }} onClick={props.onClick} onContextMenu={props.onContextMenu} className={`${MinesweepercellStyles.Cell} ${MinesweepercellStyles.Unopened}`}>
+      <div
+        index={props.index}
+        style={{ height: props.side, width: props.side }}
+        onClick={props.onClick}
+        onContextMenu={props.onContextMenu}
+        className={`${MinesweepercellStyles.Cell} 
+        ${MinesweepercellStyles.Unopened}`}>
       </div>
   }
 
