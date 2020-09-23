cbabfd022b53747eba2b3cdf74fbb72c2832a754
Pebanh <peter.banh95@gmail.com>
2020-09-23 00:23:41 -0400

    Change class to className

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className={FooterStyles.Footer}>
       <p>Made By Peter Banh</p>
-      <a href="https://www.linkedin.com/in/peter-banh/"><i class="fa fa-linkedin-square" style={{ fontSize: 25 }}></i></a>
-      <a href="https://github.com/banhpete/Minesweeper-Vs"><i class="fa fa-github-square" style={{ fontSize: 25 }}></i></a>
+      <a href="https://www.linkedin.com/in/peter-banh/"><i className="fa fa-linkedin-square" style={{ fontSize: 25 }}></i></a>
+      <a href="https://github.com/banhpete/Minesweeper-Vs"><i className="fa fa-github-square" style={{ fontSize: 25 }}></i></a>
     </div>
   );
 }
