73856bc7af050ed125568d8bb8718283430783d6
Pebanh <peter.banh95@gmail.com>
2020-09-24 13:46:27 -0400

    Remove fontawesome cdn and add fontawesome npm module.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FooterStyles from './Footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <div className={FooterStyles.Footer}>
       <p>Made By Peter Banh</p>
-      <a href="https://www.linkedin.com/in/peter-banh/"><i className="fa fa-linkedin-square" style={{ fontSize: 25 }}></i></a>
-      <a href="https://github.com/banhpete/Minesweeper-Vs"><i className="fa fa-github-square" style={{ fontSize: 25 }}></i></a>
+      <a href="https://www.linkedin.com/in/peter-banh/"> <FontAwesomeIcon size='lg' icon={faLinkedin} /></a>
+      <a href="https://github.com/banhpete/Minesweeper-Vs"> <FontAwesomeIcon size='lg' icon={faGithubSquare} /></a>
     </div>
   );
 }
